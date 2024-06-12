33f1a5872554ebbf4edaa016f2b35ffd98ef61a9
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-06-12 17:45:49 -0500

    testing of hashmap class

--- main.rb
+++ main.rb
@@ -1,2 +1,14 @@
 require_relative "lib/hash-map"
+require_relative "lib/random-name-generator"
 
+hashmap = HashMap.new(13)
+
+12.times { |i| hashmap.set(RandomName.random_first, RandomName.random_last)}
+
+
+
+
+
+
+puts hashmap
+print hashmap.entries
