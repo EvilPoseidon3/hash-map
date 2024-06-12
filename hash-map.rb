f5421dd6b49d266b90695bb4cc95431029779773
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-06-12 12:50:20 -0500

    set method made and expanding bucket  codnitions

--- hash-map.rb
+++ hash-map.rb
@@ -3,8 +3,12 @@ class HashMap
 
   def initialize(hash_map_size)
     @bucket = Array.new(hash_map_size)
-    @
+    @capacity = hash_map_size
+    @load_factor = 0.8
+    
+
   end
+
   def hash(key)
     hash_code = 0
     prime_number = 31
@@ -14,6 +18,30 @@ class HashMap
     hash_code
   end
   
+  def set(key,value)
+    @hashcode = hash(key)
+    @index = @hashcode % @capacity
+    if self[@index] == nil
+      self[@index] = "#{key} = #{value}"
+    else
+      @newBucket = Array.new(@bucket.length * 2)
+      @bucket.each { |node| @newBucket.set(node.get_key,node.get_value)}
+      @bucket = @newBucket
+    end
+
+  end
+  def get_key(node)
+    @split = node.index(" = ")
+    @key = node.split(0, @split)
+    @key
+  end
+
+  def get_value(node)
+    @split = node.index(" = ")
+    @value = node.split(@split+3, @node.length-1)
+    @value
+  end
+
   def get(key)
 
   end
